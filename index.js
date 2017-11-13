b1c45df2d4e57610c4a20ac389af89eb89eae007
Max han <mwellh84@gmail.com>
2017-11-13 23:00:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function printBadges(arr){
   for (let idx = 0; idx < arr.length; idx++) {
-    console.log(`Welcome ${arr[idx]}! You are employee #${idx + 1}`);
+    console.log(`Welcome ${arr[idx]}! You are employee #${idx + 1}.`);
     // 'Welcome ${ arr[idx]}! You are employee #${idx + 1 }.'
   }
   return arr
