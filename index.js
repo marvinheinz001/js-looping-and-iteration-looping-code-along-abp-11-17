e31a0a83f713d4e71787b03eb1eecefa191e2836
Max han <mwellh84@gmail.com>
2017-11-13 22:27:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(arrayNames){
+  for(let i = 0; i < arrayNames.length; i++){
+    console.log('Welcome ${arrayNames[i]}! You are employee #${i+1}.')
+  }
+  return arrayNames
+}
+
+
+function coinToss(){
+  return.Math.random() >= 0.5; // greater than .5 is tails
+}
+
+function tailsNeverFails(){
+  let counter = 0
+  while (cointoss() >= 0.5){
+    counter += 1;
+    cointoss();
+  }
+  return "You got ${counter} tails in a row!"
+}
