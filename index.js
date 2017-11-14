a980b47bde795bdb050e860b60216f245bae68c2
Max han <mwellh84@gmail.com>
2017-11-14 00:07:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ function printBadges(arr){
   for (let idx = 0; idx < arr.length; idx++) {
     console.log(`Welcome ${arr[idx]}! You are employee #${idx + 1}.`);
     // 'Welcome ${ arr[idx]}! You are employee #${idx + 1 }.'
+  
   }
   return arr
 }
 
-
+function coinflip(){
+  return Math.random() >= 0.5;
+}
 
 function tailsNeverFails(){
   let counter = 0;
-  while (Math.random() >= 0.5){
+  while (coinflip()){
     counter += 1;
-    Math.random();
   }
   console.log(`You got ${counter} tails in a row!`)
 }
